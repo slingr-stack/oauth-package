54491b24700ac811cffb75a7b22ebdde2ebd302b
agreggio-slingr <agreggio@slingr.io>
2023-06-26 13:07:01 -0300

    #1 Create oAuth package

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,160 +1,9 @@
 /****************************************************
- Dependencies
- ****************************************************/
-
-var httpService = svc.http;
-
-
-/****************************************************
- Public API - Generic Functions
- ****************************************************/
-
-exports.get = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.get(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.post = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.post(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.put = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.put(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.patch = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.patch(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.delete = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.delete(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.head = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.head(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.options = function (url, httpOptions, callbackData, callbacks) {
-    var options = checkHttpOptions(url, httpOptions);
-    return httpService.options(Sharepoint(options), callbackData, callbacks);
-};
-
-exports.utils = {};
-
-exports.utils.parseTimestamp = function (dateString) {
-    if (!dateString) {
-        return null;
-    }
-    var dt = dateString.split(/[: T\-]/).map(parseFloat);
-    return new Date(dt[0], dt[1] - 1, dt[2], dt[3] || 0, dt[4] || 0, dt[5] || 0, 0);
-};
-
-exports.utils.formatTimestamp = function (date) {
-    if (!date) {
-        return null;
-    }
-    var pad = function (number) {
-        var r = String(number);
-        if (r.length === 1) {
-            r = '0' + r;
-        }
-        return r;
-    };
-    return date.getUTCFullYear()
-        + '-' + pad(date.getUTCMonth() + 1)
-        + '-' + pad(date.getUTCDate())
-        + 'T' + pad(date.getUTCHours())
-        + ':' + pad(date.getUTCMinutes())
-        + ':' + pad(date.getUTCSeconds())
-        + '.' + String((date.getUTCMilliseconds() / 1000).toFixed(3)).slice(2, 5)
-        + 'Z';
-};
-
-exports.utils.fromDateToTimestamp = function (params) {
-    if (!!params) {
-        return {timestamp: new Date(params).getTime()};
-    }
-    return null;
-};
-
-exports.utils.fromMillisToDate = function (params) {
-    if (!!params) {
-        var sdf = new Intl.DateTimeFormat('en-US', {
-            year: 'numeric', month: '2-digit', day: '2-digit',
-            hour: '2-digit', minute: '2-digit', second: '2-digit',
-            timeZone: 'UTC'
-        });
-        return {date: sdf.format(new Date(parseInt(params)))};
-    }
-    return null;
-};
-
-/****************************************************
- Private helpers
- ****************************************************/
-
-var concatQuery = function (url, key, value) {
-    return url + ((!url || url.indexOf('?') < 0) ? '?' : '&') + key + "=" + value;
-}
-
-var checkHttpOptions = function (url, options) {
-    options = options || {};
-    if (!!url) {
-        if (isObject(url)) {
-            // take the 'url' parameter as the options
-            options = url || {};
-        } else {
-            if (!!options.path || !!options.params || !!options.body) {
-                // options contains the http package format
-                options.path = url;
-            } else {
-                // create html package
-                options = {
-                    path: url,
-                    body: options
-                }
-            }
-        }
-    }
-    return options;
-}
-
-var isObject = function (obj) {
-    return !!obj && stringType(obj) === '[object Object]'
-}
-
-var stringType = Function.prototype.call.bind(Object.prototype.toString)
-
-var parse = function (str) {
-    try {
-        if (arguments.length > 1) {
-            var args = arguments[1], i = 0;
-            return str.replace(/(:(?:\w|-)+)/g, () => {
-                if (typeof (args[i]) != 'string') throw new Error('Invalid type of argument: [' + args[i] + '] for url [' + str + '].');
-                return args[i++];
-            });
-        } else {
-            if (str) {
-                return str;
-            }
-            throw new Error('No arguments nor url were received when calling the helper. Please check it\'s definition.');
-        }
-    } catch (err) {
-        sys.logs.error('Some unexpected error happened during the parse of the url for this helper.')
-        throw err;
-    }
-}
-/****************************************************
  OAuth
  ****************************************************/
 exports.connectUser = function () {
     sys.ui.sendMessage({
-        scope: 'uiService:sharepoint.oAuth',
+        scope: 'uiService:oauth.oAuth',
         name: 'connectUser',
         config: {
             authUrl: config.get("authUrl"),
@@ -163,14 +12,15 @@ exports.connectUser = function () {
             clientSecret: config.get("clientSecret"),
             scope: config.get("scope"),
             state: config.get("state"),
-            callbackUrl: config.get("callbackUrl"),
+            oauthCallback: config.get("oauthCallback"),
+            id: config.get("id")
         },
         callbacks: {
             userConnected: function (originalMessage, callbackData) {
                 var config = callbackData;
                 sys.logs.error('Code: ' + JSON.stringify(config.code));
                 var res = svc.http.post({
-                    url: config.authURL,
+                    url: config.accessTokenUrl,
                     headers: {
                         Accept: "application/json",
                         "Content-Type": "application/x-www-form-urlencoded"
@@ -179,78 +29,20 @@ exports.connectUser = function () {
                         client_id: config.clientId,
                         client_secret: config.clientSecret,
                         code: config.code,
-                        redirect_uri: config.redirect_uri,
+                        redirect_uri: config.oauthCallback,
                         grant_type: "authorization_code"
                     }
                 });
-                sys.storage.put(sys.context.getCurrentUserRecord().id() +' - access_token', res.access_token);
-                sys.storage.put(sys.context.getCurrentUserRecord().id() +' - refresh_token', res.refresh_token);
-
+                if(config.id) {
+                    sys.storage.put(config.id +' - access_token', res.access_token);
+                    sys.storage.put(config.id +' - refresh_token', res.refresh_token);
+                } else {
+                    sys.logs.error('Configuration ID must be provided to store tokens ',config);
+                }
             },
             fail: function (originalMessage, callbackData) {
                 sys.logs.error('Fail callback')
             }
         }
     });
-}
-
-/****************************************************
- Constants
- ****************************************************/
-
-var SHAREPOINT_API_BASE_URL = "https://graph.microsoft.com"; // TODO: Set the base url
-var API_URL = SHAREPOINT_API_BASE_URL + ""; // TODO: Set the base url for the api
-
-/****************************************************
- Configurator
- ****************************************************/
-
-var Sharepoint = function (options) {
-    options = options || {};
-    options = setApiUri(options);
-    options = setRequestHeaders(options);
-    options = setAuthorization(options);
-    return options;
-}
-
-/****************************************************
- Private API
- ****************************************************/
-
-function setApiUri(options) {
-    var url = options.path || "";
-    options.url = API_URL + url;
-    sys.logs.debug('[sharepoint] Set url: ' + options.path + "->" + options.url);
-    delete options.path;
-    return options;
-}
-
-function setRequestHeaders(options) {
-    var headers = options.headers || {};
-    headers = mergeJSON(headers, {"Content-Type": "application/json"});
-    options.headers = headers;
-    return options;
-}
-
-function setAuthorization(options) {
-    var authorization = options.authorization || {};
-    authorization = mergeJSON(authorization, {
-        type: "oauth2",
-        accessToken: sys.storage.get(sys.context.getCurrentUserRecord().id() +' - access_token'),
-        headerPrefix: "Bearer"
-    });
-    options.authorization = authorization;
-    return options;
-}
-
-function mergeJSON(json1, json2) {
-    const result = {};
-    var key;
-    for (key in json1) {
-        if (json1.hasOwnProperty(key)) result[key] = json1[key];
-    }
-    for (key in json2) {
-        if (json2.hasOwnProperty(key)) result[key] = json2[key];
-    }
-    return result;
 }
