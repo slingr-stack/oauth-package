7deee9831db2338195a5d62478d356fff36ffff5
pasaperez <aperez@slingr.io>
2023-07-24 16:59:14 -0300

    Revert "Callback wait for ui-service"
    This reverts commit 1d5e79ceb6312e04f12ce775091dcc8b56606406.

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,62 +4,54 @@
 exports.connectUser = function (eventName) {
     sys.logs.info('Getting access token [oauth]');
     sys.logs.info('[oauth] User id: '+JSON.stringify(config.get("id")));
-
-    function waitUiMessage(callback) {
-        var res= sys.ui.sendMessage({
-            scope: 'uiService:oauth.oAuth',
-            name: 'connectUser',
-            config: {
-                authUrl: config.get("authUrl"),
-                accessTokenUrl: config.get("accessTokenUrl"),
-                clientId: config.get("clientId"),
-                clientSecret: config.get("clientSecret"),
-                scope: config.get("scope"),
-                state: config.get("state"),
-                oauthCallback: config.get("oauthCallback"),
-                id: config.get("id"),
-                http: dependencies.http._name,
-                eventName: eventName
-            },
-            callbacks: {
-                userConnected: function (originalMessage, callbackData) {
-                    sys.logs.info('[oauth] userConnected callback');
-                    var config = callbackData.data;
-                    var response = svc[config.http].post({
-                        url: config.accessTokenUrl,
-                        headers: {
-                            Accept: "application/json",
-                            "Content-Type": "application/x-www-form-urlencoded"
-                        },
-                        body: {
-                            client_id: config.clientId,
-                            client_secret: config.clientSecret,
-                            code: config.code,
-                            redirect_uri: config.oauthCallback,
-                            grant_type: "authorization_code"
-                        }
-                    });
-                    if(config.id) {
-                        sys.logs.info('Saving access token and refresh token [oauth]');
-                        sys.storage.put(config.id +' - access_token', response.access_token);
-                        sys.storage.put(config.id +' - refresh_token', response.refresh_token);
-                        if(config.eventName) {
-                            sys.events.triggerEvent(config.eventName, {configId: config.id,accessToken: response.access_token, refreshToken: response.refresh_token});
-                        }
-                    } else {
-                        sys.logs.error('Configuration id must be provided to store tokens [oauth] ',config);
+    sys.ui.sendMessage({
+        scope: 'uiService:oauth.oAuth',
+        name: 'connectUser',
+        config: {
+            authUrl: config.get("authUrl"),
+            accessTokenUrl: config.get("accessTokenUrl"),
+            clientId: config.get("clientId"),
+            clientSecret: config.get("clientSecret"),
+            scope: config.get("scope"),
+            state: config.get("state"),
+            oauthCallback: config.get("oauthCallback"),
+            id: config.get("id"),
+            http: dependencies.http._name,
+            eventName: eventName
+        },
+        callbacks: {
+            userConnected: function (originalMessage, callbackData) {
+                sys.logs.info('[oauth] userConnected callback');
+                var config = callbackData.data;
+                var response = svc[config.http].post({
+                    url: config.accessTokenUrl,
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: {
+                        client_id: config.clientId,
+                        client_secret: config.clientSecret,
+                        code: config.code,
+                        redirect_uri: config.oauthCallback,
+                        grant_type: "authorization_code"
                     }
-                },
-                fail: function (originalMessage, callbackData) {
-                    sys.logs.error('Fail callback [oauth]')
+                });
+                if(config.id) {
+                    sys.logs.info('Saving access token and refresh token [oauth]');
+                    sys.storage.put(config.id +' - access_token', response.access_token);
+                    sys.storage.put(config.id +' - refresh_token', response.refresh_token);
+                    if(config.eventName) {
+                        sys.events.triggerEvent(config.eventName, {configId: config.id,accessToken: response.access_token, refreshToken: response.refresh_token});
+                    }
+                } else {
+                    sys.logs.error('Configuration id must be provided to store tokens [oauth] ',config);
                 }
+            },
+            fail: function (originalMessage, callbackData) {
+                sys.logs.error('Fail callback [oauth]')
             }
-        });
-        callback(res);
-    }
-
-    waitUiMessage( function(res) {
-        sys.logs.info('[oauth] connectUser response ', res);
+        }
     });
 }
 
